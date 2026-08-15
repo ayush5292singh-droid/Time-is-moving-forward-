8af792933f0b188bbea1ab9a2c0254800d7f5974
Ayush Singh <ayush5292singh@gmail.com>
2026-08-15 15:37:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
 const PIN = "7890";
 
+
 function unlock() {
 
-  const entered = document.getElementById("pinInput").value;
-  const error = document.getElementById("error");
+  const entered =
+    document.getElementById("pinInput").value;
+
+  const error =
+    document.getElementById("error");
 
   if (entered === PIN) {
 
-    document.getElementById("lockScreen").style.display = "none";
-    document.getElementById("mainApp").style.display = "block";
+    error.textContent = "";
+
+    document.getElementById("lockScreen")
+      .style.display = "none";
+
+    document.getElementById("mainApp")
+      .style.display = "block";
+
+    window.scrollTo(0, 0);
 
   } else {
 
     error.textContent = "Incorrect PIN";
-    document.getElementById("pinInput").value = "";
+
+    document.getElementById("pinInput")
+      .value = "";
 
   }
+
+}
+
+
+function lockVault() {
+
+  document.getElementById("mainApp")
+    .style.display = "none";
+
+  document.getElementById("lockScreen")
+    .style.display = "flex";
+
+  document.getElementById("pinInput")
+    .value = "";
+
+  document.getElementById("error")
+    .textContent = "";
+
 }
